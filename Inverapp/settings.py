ed554a36826348ed33b74ef96adf456836b6a0e2
Pablo Conte <contepablod@gmail.com>
2022-06-28 23:09:49 -0300

    Adds Apps to settings.py

--- Inverapp/settings.py
+++ Inverapp/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'RestAPI',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
