5f64ad2902ca8a7fa9a4effccb9c4517636c786c
Pablo Conte <contepablod@gmail.com>
2022-06-28 23:10:54 -0300

    Adds Models to RestAPI/models.py

--- RestAPI/models.py
+++ RestAPI/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    Job = models.CharField(max_length=250)
+    Auto_Date = models.DateTimeField(auto_now=True)  # For data consistency
+    Date = models.CharField(max_length=10)  # For filtering
+    Done = models.BooleanField()
