12f1bef14a4e5f0242441d614b58b9da032ac5b2
MelanieZeidel <melanie.a.zeidel@gmail.com>
2023-12-17 16:36:27 -0300

    Create .venv folder again, and install pip, django and python correctly

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Leona\Desktop\exam\IT Technical Exam\todo-challenge\.venv
+set VIRTUAL_ENV=C:\Users\Lemel\Desktop\exam\todo-challenge\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
